b7e8269eece31b9f8a750db744ec80f35a2bcd2e
vensnow <timur333t@yandex.ru>
2021-01-16 23:23:32 +0500

    Добавлены Теги

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -24,6 +25,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft', verbose_name='Статус')
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         verbose_name = 'Пост'
